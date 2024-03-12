7ec4ac0c84a20c1e0ec567d3ea5497ce9ea5afc4
Prof. Me. Massaki Igarashi <162244809+professormassaki@users.noreply.github.com>
2024-03-12 16:38:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='example_pypi_package',
-    author='Tom Chen',
-    author_email='tomchen.org@gmail.com',
+    author='Massaki Igarashi',
+    author_email='massaki.igarashi@gmail.com',
     description='Example PyPI (Python Package Index) Package',
     keywords='example, pypi, package',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/tomchen/example_pypi_package',
+    url='https://github.com/professormassaki/example_pypi_package',
     project_urls={
-        'Documentation': 'https://github.com/tomchen/example_pypi_package',
+        'Documentation': 'https://github.com/professormassaki/example_pypi_package',
         'Bug Reports':
-        'https://github.com/tomchen/example_pypi_package/issues',
-        'Source Code': 'https://github.com/tomchen/example_pypi_package',
+        'https://github.com/professormassaki/example_pypi_package/issues',
+        'Source Code': 'https://github.com/professormassaki/example_pypi_package',
         # 'Funding': '',
         # 'Say Thanks!': '',
     },
